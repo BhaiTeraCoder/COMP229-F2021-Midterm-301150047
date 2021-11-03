14258db56c4c8babc4d8559a5002f1efe925cefd
bhaiteracoder <gambhirjitin@live.com>
2021-11-03 12:44:25 -0400

    Mid Term

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  "URI":"mongodb+srv://user1:kwGotl0zDgRu3i3y@cluster0.kmjsg.mongodb.net/books229?retryWrites=true&w=majority"
+};
